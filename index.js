663462563b4b4f2fea82eae4afe368dcec5e56fa
Anand Angalig <anandangalig@gmail.com>
2020-03-21 12:27:43 -0400

    update apollo-server-express to latest and remove unnecessary packages

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // parses HTTP request's body to filter out unwanted stuff
-const { graphqlExpress, graphiqlExpress, gql } = require('apollo-server-express'); // integrated GraphQL with Express
-const { makeExecutableSchema } = require('graphql-tools'); // creates the GraphQL schema
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express"); // integrated GraphQL with Express
 
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
-// Put together a schema
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-// Initialize the app
 const app = express();
+server.applyMiddleware({ app });
 
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-const typeDefs = `
+const typeDefs = gql`
   type Query {
     goals: [Goal]
   }
