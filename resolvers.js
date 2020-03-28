0105efe483325acf96d34336208e7be36a030e9b
Anand Angalig <anandangalig@gmail.com>
2020-03-28 17:06:04 -0400

    add create and read goals functionalities

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,7 @@
+const ObjectId = require("mongodb").ObjectID;
 const mongoUtils = require("./mongoUtils");
 
-const goalsResolver = async () => {
+const getGoals = async () => {
   const database = mongoUtils.getDatabase();
   const goals = await database
     .collection("goals")
@@ -10,8 +11,38 @@ const goalsResolver = async () => {
   return goals;
 };
 
+const createGoal = async (parent, args) => {
+  const database = mongoUtils.getDatabase();
+  const result = await database.collection("goals").insertOne({
+    title: args.goalInput.title,
+    cadence: args.goalInput.cadence,
+    cadenceCount: 0,
+    totalCount: 0,
+    timeStamps: [],
+  });
+
+  return result.ops[0];
+};
+const getGoalDetails = async (parent, args) => {
+  const database = mongoUtils.getDatabase();
+  const [result] = await database
+    .collection("goals")
+    .find({ _id: ObjectId(args.goalId) })
+    .toArray();
+
+  return result;
+};
+
 const resolvers = {
-  Query: { goals: goalsResolver },
+  Query: {
+    goals: getGoals,
+    getGoalDetails: getGoalDetails,
+  },
+  Mutation: {
+    createGoal: createGoal,
+    // updateGoal: updateGoal,
+    // deleteGoal: deleteGoal,
+  },
 };
 
 module.exports = resolvers;
--- schema.js
+++ schema.js
@@ -3,13 +3,24 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type Query {
     goals: [Goal]
+    getGoalDetails(goalId: String!): Goal
   }
+  type Mutation {
+    createGoal(goalInput: GoalInput): Goal
+  }
+  # General Types:
   type Goal {
+    _id: ID
     title: String
     cadence: String
     cadenceCount: Int
     totalCount: Int
     timeStamps: [String]
+  }
+  # Inputs Types:
+  input GoalInput {
+    title: String!
+    cadence: String!
   }
 `;
 
