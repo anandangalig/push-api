65f5a88e1371967fa70808ee5af571d5c7181d4c
Anand Angalig <anandangalig@gmail.com>
2020-04-19 13:40:41 -0400

    add user ID in Apollo server context and some cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const bodyParser = require("body-parser");
+const { isNil } = require("ramda");
 
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
@@ -9,19 +10,21 @@ const { userLogin, userSignUp } = require("./helpers");
 
 require("dotenv").config();
 
-// init Apollo server with GraphQL types and resolvers
-const server = new ApolloServer({ typeDefs, resolvers });
-
 // init express
 const app = express();
+// init Apollo server with GraphQL schema and context with user info
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: (request) =>
+    isNil(request.req.currentUser) ? null : { currentUserID: request.req.currentUser._id },
+});
+
 // Handle user signup and login:
 app.post("/signup", bodyParser.json(), userSignUp);
 app.get("/login", bodyParser.json(), userLogin);
-// add custom middlewares
-app.use("/graphql", verifyAndAttachJWTData, attachCurrentUser); // all operations except Signup and Login should be sent to this endpoint
-
-// connect Express with Apollo
-server.applyMiddleware({ app });
+// Add custom middlewares
+app.post("/graphql", verifyAndAttachJWTData, attachCurrentUser, server.getMiddleware()); // all operations except Signup and Login should be sent to this endpoint
 
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
